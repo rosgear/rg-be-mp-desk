af74d8eb9e20e2b8792829826c8d0155bc6a310a
Anton Tivonenko <rosgear.it@gmail.com>
2026-06-12 21:54:40 +0300

    Extension

---
+++ src/Extension.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * Расширение модуля веб-приложения RosGear.
+ * 
+ * @link https://rosgear.ru/
+ * @copyright Copyright (c) 2015 RosGear
+ * @license https://rosgear.ru/license/
+ */
+
+namespace Rg\Backend\Marketplace\Desk;
+
+/**
+ * Панель Маркетплейс.
+ * 
+ * Вывод расширений маркетплейс.
+ * 
+ * Расширение принадлежит модулю "Маркетплейс".
+ * 
+ * @author Anton Tivonenko <anton.tivonenko@gmail.com>
+ * @package Rg\Backend\Marketplace\Desk
+ * @since 1.0
+ */
+class Extension extends \Ge\Panel\Extension\Extension
+{
+    /**
+     * {@inheritdoc}
+     */
+    public string $id = 'rg.be.mp.desk';
+
+    /**
+     * {@inheritdoc}
+     */
+    public string $defaultController = 'items';
+}
